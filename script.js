2c94674fa8146994cdab79371acc2c3f71110353
Siva Durshika <sivadurshika22@gmail.com>
2025-01-16 03:17:59 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function showPopup(link) {
-    const popupContent = `<p>Click <a href="${link}" target="_blank">here</a> to visit the LinkedIn post.</p><button onclick="closePopup()">Close</button>`;
-    document.getElementById("popup").innerHTML = popupContent;
-    document.getElementById("popup").style.display = "block";
+  const popupContent = `
+    <p>Click <a href="${link}" target="_blank" rel="noopener noreferrer">here</a> to visit the LinkedIn post.</p>
+    <button onclick="closePopup()">Close</button>
+  `;
+  document.getElementById("popup-text").innerHTML = popupContent;
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
 }
